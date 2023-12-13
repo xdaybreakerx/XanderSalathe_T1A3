dd397a178e99c5bd1b3f518f932113747b881be5
xdaybreakerx <xsalathe@gmail.com>
2023-12-13 23:47:13 +1100

    Add menu and app structure

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello, World!")
+from functions import dragon_hello, create_character_sheet, dragon_goodbye
+
+dragon_hello()
+
+def app_menu():
+    print("1. Enter 1 to add a character")
+    print("2. Enter 2 to update a character")
+    print("3. Enter 3 to enter combat")
+    print("4. Enter 4 to exit\n")
+    choice = input("Enter your selection: ")
+    return choice
+
+users_choice = ""
+
+while users_choice != "4":
+    users_choice = app_menu()
+    if users_choice == "1":
+        create_character_sheet()
+    elif users_choice == "2":
+        pass
+    elif users_choice == "3":
+        pass
+    elif users_choice == "4":
+        continue
+    else:
+        print("Invalid Input - Please input 1-4")
+
+print("Thank you for using the Barbarian Dice Roller! \ngood bye!")
+
+dragon_goodbye()
