e23e74ea013f59d041a6b3178665d73b04c92e36
xdaybreakerx <xsalathe@gmail.com>
2023-12-13 23:29:46 +1100

    Update roll_to_hit function to return Critical if d20 = 20

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ import d20
 import colored
 import math
 
+
 def dragon_hello():
     print(
         r"""
@@ -94,9 +95,15 @@ def create_character_sheet():
 
         pc_proficiency_bonus = math.ceil(1 + (pc_level * 0.25))
 
-        print(f"\nThanks for that {pc_name}. To confirm everything: \nYou are level {pc_level} with a strength modifier of {pc_strength_mod}. \n")
-        print(f"Based on your level, your rage bonus is {rage_bonus} and your proficiency bonus is {pc_proficiency_bonus}. \n")
-        print(f"Finally - like a true Barbarian you wield a {pc_weapon} to strike fear into the hearts of your enemies. \n")
+        print(
+            f"\nThanks for that {pc_name}. To confirm everything: \nYou are level {pc_level} with a strength modifier of {pc_strength_mod}. \n"
+        )
+        print(
+            f"Based on your level, your rage bonus is {rage_bonus} and your proficiency bonus is {pc_proficiency_bonus}. \n"
+        )
+        print(
+            f"Finally - like a true Barbarian you wield a {pc_weapon} to strike fear into the hearts of your enemies. \n"
+        )
 
         char_info_check = input(str("Is this all correct? \n")).lower()
         if char_info_check[0] == "n":
@@ -119,50 +126,59 @@ def create_character_sheet():
         "weapon": pc_weapon,
     }
 
-    print(char_sheet) #this is used for troubleshooting and will be removed in final
+    print(char_sheet)  # this is used for troubleshooting and will be removed in final
 
     # Write dictionary to file
     with open("char_sheet.json", "w") as json_file:
         json.dump(char_sheet, json_file)
 
+
 def roll_to_hit():
     # load data from JSON
-    with open('char_sheet.json', 'r') as f:
+    with open("char_sheet.json", "r") as f:
         character_sheet = json.load(f)
-    
-    pc_strength_mod = character_sheet['strength_mod']
-    pc_proficiency_bonus = character_sheet['proficiency']
-    pc_weapon = character_sheet['weapon']
-    
-    print(f"PC strength mod from JSON file is {pc_strength_mod}. PC prof bonus is {pc_proficiency_bonus}")
-    
+
+    critical = False
+    pc_strength_mod = character_sheet["strength_mod"]
+    pc_proficiency_bonus = character_sheet["proficiency"]
+    pc_weapon = character_sheet["weapon"]
+
     while True:
-        roll_modifier = input("Do you have (a)dvantage, (d)isadvantage or is it a (n)ormal roll?\n").lower()
-    
+        roll_modifier = input(
+            "Do you have (a)dvantage, (d)isadvantage or is it a (n)ormal roll?\n"
+        ).lower()
+
         if roll_modifier.startswith("a"):
-            roll_value = "2d20kh1" # roll 2x d20, keep highest 1
+            roll_value = "2d20kh1"  # roll 2x d20, keep highest 1
             break
         elif roll_modifier.startswith("d"):
-            roll_value = "2d20kl1" # roll 2x d20, keep lowest 1
+            roll_value = "2d20kl1"  # roll 2x d20, keep lowest 1
             break
         elif roll_modifier.startswith("n"):
-            roll_value = "1d20" # roll 1x d20
+            roll_value = "1d20"  # roll 1x d20
+            break
+        elif roll_modifier.startswith("t"):
+            roll_value = (
+                "20"  # rolls 20 - t for testing, will be removed in final version
+            )
             break
         else:
-            print("Invalid input. Please enter (a)dvantage, (d)isadvantage, or is it a (n)ormal roll?")    
-            
+            print(
+                "Invalid input. Please enter (a)dvantage, (d)isadvantage, or is it a (n)ormal roll?"
+            )
+
     roll_natural = d20.roll(roll_value)
     roll_total = roll_natural.total + pc_strength_mod + pc_proficiency_bonus
-    
-    print(f"{roll_natural} is the die value, {roll_total} is the final value")
-    
-    if roll_natural == 20:
+
+    if roll_natural.total == 20:
+        critical = True
         message = "*** A natural twenty! ***\n"
-    elif roll_natural == 1:
+    elif roll_natural.total == 1:
         message = "*** A nautral one! ***"
+        # critical = False
     else:
         message = f"You roll a {roll_total} to hit! Confirm you hit enemy AC."
-        
+        # critical = False
+
     print(f"\nYou swing your {pc_weapon} - {message}\n")
-    
-roll_to_hit()
+    return critical
