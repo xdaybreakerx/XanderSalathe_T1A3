a1450034a0b24d597e6d8ea52ed66375373a0e2d
xdaybreakerx <xsalathe@gmail.com>
2023-12-14 23:35:43 +1100

    Add error handling for main program for KeyboardInterrupt, add error handling for FileNotFound error in combat function

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 from functions import dragon_hello, create_character_sheet, update_character_sheet_menu, combat, dragon_goodbye
 
-dragon_hello()
+def main():
+    try:
+        dragon_hello()
 
-def app_menu():
-    print("1. Enter 1 to add a character")
-    print("2. Enter 2 to update a character")
-    print("3. Enter 3 to enter combat")
-    print("4. Enter 4 to exit\n")
-    choice = input("Enter your selection: ")
-    return choice
+        def create_menu():
+            print("1. Enter 1 to add a character")
+            print("2. Enter 2 to update a character")
+            print("3. Enter 3 to enter combat")
+            print("4. Enter 4 to exit\n")
+            choice = input("Enter your selection: ")
+            return choice
 
-users_choice = ""
+        users_choice = ""
 
-while users_choice != "4":
-    users_choice = app_menu()
-    if users_choice == "1":
-        create_character_sheet()
-    elif users_choice == "2":
-        update_character_sheet_menu()
-    elif users_choice == "3":
-        combat()
-    elif users_choice == "4":
-        continue
-    else:
-        print("Invalid Input - Please input 1-4")
+        while users_choice != "4":
+            users_choice = create_menu()
+            if users_choice == "1":
+                create_character_sheet()
+            elif users_choice == "2":
+                update_character_sheet_menu()
+            elif users_choice == "3":
+                combat()
+            elif users_choice == "4":
+                continue
+            else:
+                print("Invalid Input - Please input 1-4")
 
-print("Thank you for using the Barbarian Dice Roller! \ngood bye!")
+        print("Thank you for using the Barbarian Dice Roller! \ngood bye!")
 
-dragon_goodbye()
+        dragon_goodbye()
+        
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt caught. Exiting the program.")
+        dragon_goodbye()
+
+main()
